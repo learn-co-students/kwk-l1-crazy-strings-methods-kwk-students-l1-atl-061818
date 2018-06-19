a3fb78939d77e09300c2ae08105fdba3129cd722
dadamu4 <bessiadamu@gmail.com>
2018-06-19 14:44:24 +0000

    Done.

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,12 +1,8 @@
 # write your method here
 
 def crazy_strings(a,b)
-  puts "What is a?"
-  puts "What is b?"
-  a = gets.chomp.upcase.reverse 
-  b = gets.chomp.swap.gsub("s", "z")
+  a.reverse.upcase + " " + b.swapcase.gsub("s", "z")
   
-  puts "#{a}, #{b}"
 end
 
-crazy_strings(a,b)
+#crazy_strings
